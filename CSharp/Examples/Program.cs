c801e33511e410ee68a726fb5818579b3bcd44a2
Folleach <32067915+Folleach@users.noreply.github.com>
2020-12-21 16:07:44 +0500

    Create client configuration

--- CSharp/Examples/Program.cs
+++ CSharp/Examples/Program.cs
@@ -15,10 +15,21 @@ namespace Examples
         static void Main(string[] args)
         {
             if (args.Length == 0)
-                ServerExample.Start(Port);
-            else
+            {
+#if CLIENT
                 ClientExample.Start(Port);
-            
+#else
+                ServerExample.Start(Port);
+#endif
+            }
+            else
+            {
+                if (args[0] == "client")
+                    ClientExample.Start(Port);
+                else
+                    ServerExample.Start(Port);
+            }
+
             while (true)
             {
                 Console.ReadLine();
