7e1afd83898e8a0be980040e4d6a9b3aa892fb03
Folleach <32067915+Folleach@users.noreply.github.com>
2020-12-21 16:28:22 +0500

    Create Int32DataWrapper

---
+++ CSharp/SocketFlow/DataWrappers/Int32DataWrapper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SocketFlow.DataWrappers
+{
+    public class Int32DataWrapper : IDataWrapper<int>
+    {
+        public int FormatRaw(byte[] data)
+        {
+            return BitConverter.ToInt32(data, 0);
+        }
+
+        public byte[] FormatObject(object value)
+        {
+            return BitConverter.GetBytes((int)value);
+        }
+    }
+}
