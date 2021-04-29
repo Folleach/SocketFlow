c4906e09f2b93b4b0fe6bc3991cc24eec5d4b9bd
Folleach <32067915+Folleach@users.noreply.github.com>
2021-04-29 19:37:15 +0500

    Remove thread from FlowClient

--- CSharp/SocketFlow.Client/FlowClient.cs
+++ CSharp/SocketFlow.Client/FlowClient.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Net;
 using System.Net.Sockets;
-using System.Threading;
 using SocketFlow.DataWrappers;
 
 namespace SocketFlow.Client
@@ -14,7 +13,6 @@ namespace SocketFlow.Client
         private readonly FlowOptions options;
         private readonly FlowBinder flowBinder;
         private TcpProtocol protocol;
-        private Thread thread;
 
         public event Action<FlowClient> Disconnected;
         public event Action<FlowClient> Connected;
@@ -37,8 +35,7 @@ namespace SocketFlow.Client
             protocol.OnClose += Protocol_OnClose;
             protocol.OnData += Protocol_OnData;
 
-            thread = new Thread(protocol.Reader) { IsBackground = true };
-            thread.Start();
+            protocol.Reader();
             Connected?.Invoke(this);
         }
 
